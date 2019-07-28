5c82c1d369283cc231df27c1ee7b44108b77bb10
Mike Duskis <mike.duskis@cybergrx.com>
2019-07-28 15:19:20 -0600

    First attempt to deploy to PyPi using Travis CI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from unittest.runner import TextTestRunner
 MAJOR_VERSION = 2
 # An even MINOR_VERSION number indicates a public release
 MINOR_VERSION = 6
-PATCH_VERSION = 3
+PATCH_VERSION = 4
 
 
 class Tester(test):
