bf51c77c3c9b30fdb021edeaa07052f08c066cf7
Mike Duskis <mike.duskis@cybergrx.com>
2019-12-03 10:23:00 -0700

    Added build diagnostics

--- build_packages.sh
+++ build_packages.sh
@@ -13,14 +13,17 @@ export QUESTIONS_THREE_PATCH_VERSION=`cat setup.py | grep 'PATCH_VERSION =' | aw
 
 # We need to install questions-three so we can run unit tests on optional packages
 if ! $PYTHON setup.py develop; then
+  echo 'Failed to install questions-three'
   exit 1
 fi
 
 if ! $PYTHON setup.py test; then
+  echo 'Tests for questions-three failed'
   exit 1
 fi
 
 if ! $PYTHON setup.py sdist bdist_wheel -d $DIST_DIR; then
+  echo 'Build for questions-three failed'
   exit 1
 fi
 
@@ -28,9 +31,11 @@ for PACKAGE_DIR in optional_packages/*; do
   echo "Building $PACKAGE_DIR"
   cd $BASE_DIR/$PACKAGE_DIR
   if ! $PYTHON setup.py test; then
+    echo "Tests for $PACKAGE_DIR failed"
     exit 1
   fi
   if ! $PYTHON setup.py sdist bdist_wheel -d $DIST_DIR; then
+    echo "Build for $PACKAGE_DIR failed"
     exit 1
   fi
 done
